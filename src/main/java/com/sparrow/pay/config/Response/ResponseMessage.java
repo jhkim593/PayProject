617487bec10fede48bea51db842ff54d2415cc72
jhkim593 <fpdlwjzlr@naver.com>
2021-10-25 14:31:47 +0900

    feat(DefaultResponse): DefalutResponse를 이용해 response 기준 설정

---
+++ src/main/java/com/sparrow/pay/config/Response/ResponseMessage.java
@@ -0,0 +1,25 @@
+package com.sparrow.pay.config.Response;
+
+
+
+public class ResponseMessage {
+
+    public static final String PAY_SUCCESS="결제가 성공적으로 처리되었습니다.";
+    public static final String PAY_FAIL="결제 오류가 발생했습니다.";
+    public static final String PAY_CANCEL_SUCCESS="결제 취소가 정상적으로 처리되었습니다";
+    public static final String PAY_NOT_FOUND="기존 결제 정보를 찾을 수 없습니다.";
+
+
+
+    public static final String PAY_CANCEL_EXCEED="취소금액이 결제금액을 초과했습니다.";
+    public static final String PAY_CANCEL_VAT_EXCEED="취소부가세가 결제부가세를 초과했습니다.";
+    public static final String PAY_CANCEL_VAT_PAY_EXCEED="부가세가 결제금액을 초과했습니다.";
+    public static final String PAY_CANCEL_FAIL="결제 취소 오류가 발생했습니다.";
+
+    public static final String PAY_FIND_SUCCESS="결제 정보 조회에 성공했습니다.";
+    public static final String PAY_FIND_FAIL="결제 정보 조회 오류가 발생했습니다.";
+
+
+
+
+}
--- src/main/java/com/sparrow/pay/controller/PayController.java
+++ src/main/java/com/sparrow/pay/controller/PayController.java
@@ -1,27 +1,20 @@
 package com.sparrow.pay.controller;
 
+import com.sparrow.pay.config.Response.DefaultResponseVO;
+import com.sparrow.pay.config.Response.ResponseMessage;
 import com.sparrow.pay.dto.CancelPayRequestDto;
 import com.sparrow.pay.dto.PayRequestDto;
-import com.sparrow.pay.dto.PayResponseDto;
 import com.sparrow.pay.exception.ExceedCancelPayException;
 import com.sparrow.pay.exception.ExceedVatException;
 import com.sparrow.pay.exception.PayNotFoundException;
 import com.sparrow.pay.exception.VatExceedPriceException;
 import com.sparrow.pay.service.PayService;
 import lombok.RequiredArgsConstructor;
-import org.apache.commons.codec.EncoderException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.validation.Valid;
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidAlgorithmParameterException;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 
 @RestController
 @RequiredArgsConstructor
@@ -32,39 +25,38 @@ public class PayController {
     @PostMapping("/pay")
     public ResponseEntity pay(@RequestBody @Valid PayRequestDto requestDto){
         try {
-            return new ResponseEntity( payService.createPay(requestDto).getPayId(), HttpStatus.CREATED);
+            return new ResponseEntity(DefaultResponseVO.res(ResponseMessage.PAY_SUCCESS,true,payService.createPay(requestDto).getPayId()),HttpStatus.CREATED);
         } catch (Exception e){
-            e.printStackTrace();
-            return new ResponseEntity( "결제 오류", HttpStatus.CREATED);
+            return new ResponseEntity(DefaultResponseVO.res( ResponseMessage.PAY_FAIL,false),HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @PostMapping("/pay/cancel")
     public ResponseEntity cancelPay(@RequestBody @Valid CancelPayRequestDto requestDto){
         try {
-            return new ResponseEntity( payService.createCancelPay(requestDto).getPayId(), HttpStatus.CREATED);
+            return new ResponseEntity(DefaultResponseVO.res(ResponseMessage.PAY_CANCEL_SUCCESS,true,payService.createCancelPay(requestDto).getPayId()), HttpStatus.CREATED);
         }catch (PayNotFoundException e){
-            return new ResponseEntity( "기존 결제 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+            return new ResponseEntity(DefaultResponseVO.res(ResponseMessage.PAY_NOT_FOUND,false), HttpStatus.NOT_FOUND);
         } catch (ExceedCancelPayException e){
-            return new ResponseEntity( "취소금액이 결제금액을 초과했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity(DefaultResponseVO.res(ResponseMessage.PAY_CANCEL_EXCEED,false), HttpStatus.INTERNAL_SERVER_ERROR);
         } catch (ExceedVatException e){
-            return new ResponseEntity( "취소부가세가 결제부가세를 초과했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity(DefaultResponseVO.res( ResponseMessage.PAY_CANCEL_VAT_EXCEED,false), HttpStatus.INTERNAL_SERVER_ERROR);
         }catch (VatExceedPriceException e){
-            return new ResponseEntity( "부가세가 결제금액을 초과했습니다.", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity(DefaultResponseVO.res( ResponseMessage.PAY_CANCEL_VAT_PAY_EXCEED,false), HttpStatus.INTERNAL_SERVER_ERROR);
         }catch (Exception e){
-            return new ResponseEntity( "결제 취소 오류", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity( DefaultResponseVO.res(ResponseMessage.PAY_CANCEL_FAIL,false) ,HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 
     @GetMapping("/pay/{id}")
     public ResponseEntity findPay(@PathVariable String id){
         try{
-            return new ResponseEntity(payService.findPay(id), HttpStatus.OK);
+            return new ResponseEntity(DefaultResponseVO.res(ResponseMessage.PAY_FIND_SUCCESS,true,payService.findPay(id)), HttpStatus.OK);
         }catch (PayNotFoundException e){
-            return new ResponseEntity( "기존 결제 정보를 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+            return new ResponseEntity( DefaultResponseVO.res(ResponseMessage.PAY_NOT_FOUND,false), HttpStatus.NOT_FOUND);
         }catch (Exception e){
             e.printStackTrace();
-            return new ResponseEntity( "결제 정보 조회 오류", HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity(DefaultResponseVO.res( ResponseMessage.PAY_FIND_FAIL,false), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
 }
